d358a0bef7e9e114dfe57f42a6cbcb446890d511
ChytryJohn68 <julianeq121@gmail.com>
2024-09-13 15:39:21 +0200

    update

--- zadania/syntax.php
+++ zadania/syntax.php
@@ -1,8 +1,24 @@
 <?php
 
-$a= "Tu"
-$b = "funkcja"
-$c = "echo"
 echo "Tu funkcja echo";
-echo $a. $b .$c;
+echo "<br>";
+echo 'Tu funkcja echo';
+echo "<br>";
+echo ('Tu funkcja echo');
+echo "<br>";
+echo ("Tu funkcja echo");
+echo "<br>";
+print("Zgłasza sie funkcja print");
+echo "<br>";
+print('Zgałasza się funkcja print');
+echo "<br>";
+//komentarz jedno liniowy pierwszy
+#komentarz jedno liniowy drugi
+
+/*
+komentarz wieloliniowy pierwszy
+*/
 ?>
+<?='Hej'?>
+<?='<br>'?>
+<?='jeH'?>
