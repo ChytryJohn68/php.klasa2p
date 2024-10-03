77b496c2d6f9f9fadd5f953cdb2d85b682390963
ChytryJohn68 <julianeq121@gmail.com>
2024-10-03 14:13:12 +0200

    aa

--- zadania/zadanie4.php
+++ zadania/zadanie4.php
@@ -4,36 +4,39 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Document</title>
-<style>
-td{
-border:2px, solid black;
-padding: 20px;
-}
-table{
-border-collapse: collapse;
-}
-</style>
+    <style>
+        td{
+        border:2px, solid black;
+        padding: 20px;
+        }
+        table{
+        border-collapse: collapse;
+        }
+    </style>
 </head>
 <body>
 <?php
-$i=0;
-?>
+    $i=0;
+    ?>
 <table>
-<?php
-$dni=array("pon","wto","śro","czw","pią","sob","nie");
-foreach($dni as $dzien)
-if($i%2==0){
-echo'<td style="background-color: cyan;" > ';
-echo $dzien;
-echo "</td>";
-$i++;
-}else{
-echo"<td>";
-echo $dzien;
-echo"</td>";
-$i++;
-}
-?>
+    <?php
+        $dni=array("pon","wto","śro","czw","pią","sob","nie");
+        foreach($dni as $dzien)
+            if($i%2==0)
+            {
+                echo'<td style="background-color: cyan;" > ';
+                echo $dzien;
+                echo "</td>";
+                $i++;
+            }
+            else
+            {
+                echo"<td>";a
+                echo $dzien;
+                echo"</td>";
+                $i++;
+            }
+    ?>
 </table>
 
 </body>
