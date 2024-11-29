9ac0b621fbe9a4a39f12e591a4c54635ca857229
ChytryJohn68 <julianeq121@gmail.com>
2024-11-29 15:37:21 +0100

    a

---
+++ php/zadania_klasy/7.liczenie/policz_wyrazy.php
@@ -0,0 +1,10 @@
+<?php
+class PoliczWyraz{
+    public static string $tekst = "Metoda policzWyrazy() zwraca liczbę wyrazów w danym tekście";
+    public static function policzWyrazy(string $a):int
+    {
+        $slowa = explode(" ", $a); 
+        return count($slowa);
+    }
+}
+?>
