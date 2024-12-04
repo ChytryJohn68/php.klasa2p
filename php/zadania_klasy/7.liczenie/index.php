792ed0f34c3eadaa519281b4b3f7677b1c501c36
ChytryJohn68 <julianeq121@gmail.com>
2024-12-04 20:07:39 +0100

    s

--- php/zadania_klasy/7.liczenie/index.php
+++ php/zadania_klasy/7.liczenie/index.php
@@ -3,15 +3,17 @@ include_once("policz_wyrazy.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style></style>
+
 </head>
+
 <body>
-    <p><?=PoliczWyraz::$tekst?></p>
-    
+    <p><?= PoliczWyraz::$tekst ?></p>
+
     <fieldset>
         <legend>Przykładowy tekst</legend>
         <pre>
@@ -21,6 +23,10 @@ Kto cię stracił. Dziś piękność twą w całej ozdobie
 Widzę i opisuję, bo tęsknię po tobie.
         </pre>
     </fieldset>
-    <p>Tekst w ramce składa się z <?=PoliczWyraz::policzwyrazy("Litwo! Ojczyzno moja! ty jesteś jak zdrowie. Ile cię trzeba cenić, ten tylko się dowie, Kto cię stracił. Dziś piękność twą w całej ozdobie Widzę i opisuję, bo tęsknię po tobie.")?> wyrazów</p>
+    <p>Tekst w ramce składa się z
+        <?= PoliczWyraz::policzwyrazy("Litwo! Ojczyzno moja! ty jesteś jak zdrowie. Ile cię trzeba cenić, ten tylko się dowie, Kto cię stracił. Dziś piękność twą w całej ozdobie Widzę i opisuję, bo tęsknię po tobie.") ?>
+        wyrazów
+    </p>
 </body>
+
 </html>
--- php/zadania_klasy/7.liczenie/policz_wyrazy.php
+++ php/zadania_klasy/7.liczenie/policz_wyrazy.php
@@ -1,9 +1,10 @@
 <?php
-class PoliczWyraz{
+class PoliczWyraz
+{
     public static string $tekst = "Metoda policzWyrazy() zwraca liczbę wyrazów w danym tekście";
-    public static function policzWyrazy(string $a):int
+    public static function policzWyrazy(string $a): int
     {
-        $slowa = explode(" ", $a); 
+        $slowa = explode(" ", $a);
         return count($slowa);
     }
 }
