5d9ab06c8f58e58899585bb0bac4f10abfce62f6
ChytryJohn68 <julianeq121@gmail.com>
2025-02-06 14:58:53 +0100

    d

--- php/zadania_formularze/1.zadaniev2.php
+++ php/zadania_formularze/1.zadaniev2.php
@@ -1,25 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-  </head>
-  <body>
-    <fieldset border-width="2px">
-        <legend>Roczniki</legend>
-      <?php
-      $rok=2010;
-        for($i=0;$i<=15;$i++){
-          ?>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Roczniki</title>
+  <link rel="icon" href="https://pl.freepik.com/ikona/szklarnia_5563175#fromView=family&page=1&position=1&uuid=1c025c4c-9ffd-4c94-9433-fa8dc5890a68" type="image/x-icon">
+
+</head>
+
+<body>
+  <fieldset border-width="2px">
+    <legend>Roczniki</legend>
+    <?php
+    $rok = 2010;
+    for ($i = 0; $i <= 15; $i++) {
+    ?>
             <input type="radio" id="a<?=$rok?>" name="rocznik">
             <label for="a<?=$rok?>"><?=$rok?></label>
-            <br>
-          <?php
-          $rok++;
-        }
+      <br>
+    <?php
+      $rok++;
+    }
     ?>
-    </fieldset>
-  </body>
+  </fieldset>
+</body>
+
 </html>
