20c1e8066c1708fbba94a30472c566c6fe63756f
ChytryJohn68 <julianeq121@gmail.com>
2025-04-29 00:38:31 +0200

    sda

---
+++ testowanie/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css.css">
+    <link href="zdjecia/sushi_icon.png" rel="icon" type="image/x-icon" alt="LOGO">
+    <title>SUSHI BAR</title>
+</head>
+
+<body>
+    <header>
+        <img src="zdjecia/sushi_icon.png" alt="LOGO">
+        <div class="header">
+            <a href="index.php">HOME</a>
+            <a href="oferta.php">OFERTA</a>
+            <a href="menu.php">MENU</a>
+        </div>
+
+    </header>
+    <main>
+
+    </main>
+</body>
+
+</html>
