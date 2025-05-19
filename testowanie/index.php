442170790c349d142574b093b80711d3ccdc0c56
ChytryJohn68 <julianeq121@gmail.com>
2025-05-19 21:32:50 +0200

    a

--- testowanie/index.php
+++ testowanie/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css.css">
+    <link rel="stylesheet" href="formatowanie.css">
     <link href="zdjecia/sushi_icon.png" rel="icon" type="image/x-icon" alt="LOGO">
     <title>SUSHI BAR</title>
 </head>
@@ -13,9 +13,9 @@
     <header>
         <img src="zdjecia/sushi_icon.png" alt="LOGO">
         <div class="header">
-            <a href="index.php">HOME</a>
-            <a href="oferta.php">OFERTA</a>
-            <a href="menu.php">MENU</a>
+            <button href="index.php">HOME</button>
+            <button href="oferta.php">OFERTA</button>
+            <button href="menu.php">MENU</button>
         </div>
 
     </header>
--- testowanie/oferta.php
+++ testowanie/oferta.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css.css">
+    <link rel="stylesheet" href="formatowanie.css">
     <link href="zdjecia/sushi_icon.png" rel="icon" type="image/x-icon" alt="LOGO">
     <title>SUSHI BAR</title>
 </head>
